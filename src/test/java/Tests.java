f1e427abc71467e9e5e8040b3c0bb2647e5e9e70
Marina Permyakova <marpermyakova@yotateam.com>
2021-12-22 23:17:44 +0300

    1111

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,15 +1,19 @@
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Tests {
@@ -30,65 +34,97 @@ public class Tests {
     @FindBy(xpath = "//*[@id='wpcf7-f47-o1']/form/p[2]/input")
     private WebElement button;
 
-    @FindBy(xpath = "//*[contains(@class, 'wpcf7-form-control-wrap name-form')]")
+
+    //form 2
+
+    @FindBy(xpath = "//a[contains(@class, 'btn-calc popup')]")
     private WebElement userMenu;
 
-    public  Tests(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
+    @FindBy(xpath = "//*[contains(@class, 'wpcf7-form-control-wrap name-form')]/input")
+    private WebElement userForm2;
+
+    @FindBy(xpath = "//*[contains(@class, 'wpcf7-form-control-wrap tel-form')]/input")
+    private WebElement telephone2;
+
+    @FindBy(xpath = "//*[contains(@class, 'check-label')]")
+    private WebElement personalDate2;
+
+    @FindBy(xpath = "//*[contains(@class, 'wpcf7-form-control wpcf7-submit form-btn send-form')]")
+    private WebElement button2;
+
+    //   public  Tests(WebDriver driver) {
+//        PageFactory.initElements(driver, this);
+//    }
 
 
     @Before
     public void setup() {
+
+
         //определение пути до драйвера и его настройка
         System.setProperty("webdriver.chrome.driver", ReadVariable.getProperty("chromedriver"));
         //создание экземпляра драйвера
         driver = new ChromeDriver();
         //окно разворачивается на полный экран
-        driver.manage().window().maximize();
+        driver.manage().window();
         //задержка на выполнение теста = 10 сек.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //получение ссылки на страницу входа из файла настроек
-        driver.get(ReadVariable.getProperty("windowsPage"));
+
+    }
+
+    @After
+    public void close() {
+        driver.close();
     }
 
     @Test
-    public void loginTest() {
-        //значение login/password берутся из файла настроек по аналогии с chromedriver
-//и loginpage
-//вводим логин
+    public void form1() throws InterruptedException {
+        driver.get(ReadVariable.getProperty("windowsPage"));
+        PageFactory.initElements(driver, this);
 
-//        inputName(driver.findElement(By.xpath("//*[@id='wpcf7-f47-o1']/form/p[1]/span[1]/input")),
-//                ReadVariable.getProperty("name"));
+
 
         inputName(name, ReadVariable.getProperty("name"));
         inputName(telephone, ReadVariable.getProperty("telephone"));
-
-//        inputName(driver.findElement(By.xpath("//*[@id='wpcf7-f47-o1']/form/p[1]/span[2]/input")),
-//                ReadVariable.getProperty("telephone"));
-
-        //нажимаем кнопку входа
-//        clickLoginBtn(driver.findElement(By.xpath("//*[@id='wpcf7-f47-o1']/form/p[2]/label/span[2]")));
         clickLoginBtn(personalDate);
-        //вводим пароль
-    //    inputTelephone(ReadVariable.getProperty("telephone"));
-
-        //нажимаем кнопку входа
-//        clickLoginBtn(driver.findElement(By.xpath("//*[@id='wpcf7-f47-o1']/form/p[2]/input")));
         clickLoginBtn(button);
-        //нажимаем кнопку входа
-      //  clickLoginBtn();
-        //получаем отображаемый логин
+        WebElement spasibo = driver.findElement(By.xpath("//*[@id='pum_popup_title_760']"));
+        Thread.sleep(1000L);
 
-                WebElement spasibo = driver.findElement(By.xpath("//span[contains(text(), 'Спасибо, ваша заявка принята!')]"));
-        //и сравниваем его с логином из файла настроек
-        Assert.assertNotNull(spasibo); }
+        Assert.assertEquals("Спасибо, ваша заявка принята!", spasibo.getText());
+    }
+
+    @Test
+    public void form2() throws InterruptedException {
+        driver.get(ReadVariable.getProperty("windowsPage"));
+        PageFactory.initElements(driver, this);
+
+        clickLoginBtn(userMenu);
+        Thread.sleep(500L);
+
+        inputName(userForm2, ReadVariable.getProperty("name2"));
+        inputName(telephone2, ReadVariable.getProperty("telephone"));
+        clickLoginBtn(personalDate2);
+        clickLoginBtn(button2);
+        WebElement spasibo = driver.findElement(By.xpath("//*[@id='pum_popup_title_760']"));
+        Thread.sleep(500L);
+
+        Assert.assertEquals("Спасибо, ваша заявка принята!", spasibo.getText());
+    }
 
 
     public void inputName(WebElement el, String login) {
-        el.sendKeys(login); }
+        el.sendKeys(login);
+    }
 
     public void clickLoginBtn(WebElement el) {
-        el.click(); }
+        try {
+            el.click();
+        } catch (Exception e) {
+            new Actions(driver).sendKeys(Keys.PAGE_DOWN).perform();
+            el.click();
+        }
+    }
 
 }
